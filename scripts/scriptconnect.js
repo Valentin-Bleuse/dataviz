8b9ca52e58744704d4f03662ff0f6d5e2d55ad6e
Valentin-Bleuse <98829094+Valentin-Bleuse@users.noreply.github.com>
2022-10-10 19:41:30 +0200

    adding map

--- scripts/scriptconnect.js
+++ scripts/scriptconnect.js
@@ -1,7 +1,31 @@
 function RedirectionStats() {
     document.location.href = "../";
 }
+let zoom = 20
+function zooming(lat, long) {
+    for (let index = 0; index < 5; index++) {
+        setTimeout(() => {
+            positions(lat, long, zoom);
+            zoom -= 3;
+        }, 2000)
 
+
+    }
+}
+
+function positions(lat, long, lader) {
+    console.log(lat);
+    console.log(long)
+    console.log(long + lat)
+
+    var map = L.map('map').setView([lat, long], lader);
+    L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
+        maxZoom: 19,
+        attribution: '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>'
+    }).addTo(map);
+
+    var marker = L.marker([lat, long]).addTo(map);
+}
 
 function validateForm() {
     var u = document.querySelector(".username");
@@ -17,6 +41,30 @@ function validateForm() {
 
     }
     else {
-        RedirectionStats()
+        if (navigator.geolocation) {
+            /* geolocation is available */
+            document.querySelector(".container-connection").style.display = "none"
+            document.querySelector("#map").style.display = "flex"
+            navigator.geolocation.getCurrentPosition((position) => {
+                zooming(position.coords.latitude, position.coords.longitude);
+            });
+
+
+
+
+
+
+
+
+        } else {
+            /* geolocation IS NOT available */
+        }
+
+        // RedirectionStats()
     }
+
+
+
+
+
 }
