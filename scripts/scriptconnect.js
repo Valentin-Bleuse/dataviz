fe64b8ca785bfb95ffef7b1e7e7445c6315bb46a
Valentin-Bleuse <98829094+Valentin-Bleuse@users.noreply.github.com>
2022-10-17 16:02:08 +0200

    nzw map + bug

--- scripts/scriptconnect.js
+++ scripts/scriptconnect.js
@@ -57,16 +57,29 @@ function positions(lat, long, lader) {
         container._leaflet_id = null;
     }
     var map = L.map('map').setView([lat, long], lader);
-    L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
+
+    L.tileLayer('https://tiles.stadiamaps.com/tiles/alidade_smooth_dark/{z}/{x}/{y}{r}.png', {
         maxZoom: 19,
-        attribution: '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>'
+        attribution: '&copy; <a href="https://stadiamaps.com/">Stadia Maps</a>, &copy; <a href="https://openmaptiles.org/">OpenMapTiles</a> &copy; <a href="http://openstreetmap.org">OpenStreetMap</a> contributors'
     }).addTo(map);
 
-    var marker = L.marker([lat, long]).addTo(map);
+
+
+    var greenIcon = L.icon({
+        iconUrl: 'leaf-green.png',
+
+        iconSize: [38, 95], // size of the icon
+        shadowSize: [50, 64], // size of the shadow
+        iconAnchor: [22, 94], // point of the icon which will correspond to marker's location
+        shadowAnchor: [4, 62],  // the same for the shadow
+        popupAnchor: [-3, -76] // point from which the popup should open relative to the iconAnchor
+    });
+
+    var marker = L.marker([lat, long], { icon: greenIcon }).addTo(map);
     fin += 1;
     console.log(fin)
     if (fin >= 8) {
-        setTimeout(active_typing, 2000);
+        setTimeout(active_typing, 3000);
     }
 
 }
