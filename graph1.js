f3ce13cc76b4d515ec2172b478ad802e11e4b9f4
Shyronax <oscarfpoisson@gmail.com>
2022-10-19 08:47:18 +0200

    update graph

--- graph1.js
+++ graph1.js
@@ -1,5 +1,5 @@
-let width = 1500;
-let height = 1000;
+let width = "100%";
+let height = "100%";
 let facteur1 = 100;
 let facteur2 = 50;
 
@@ -7,57 +7,75 @@ let facteur2 = 50;
 
 let svg = d3.select('#graph1')
     .append('svg')
-    .attr('width', width)
-    .attr('height', height)
+        .attr('width', width)
+        .attr('height', height)
+        .attr('viewBox', '0 0 700 700')
     .append('g')
+    .attr('width', "100%")
+    .attr('height', '100%')
     .attr("transform",
-        "translate(50,50)");
+          "translate(50,50)");
 
-d3.json('/stats.json', function (data) {
+d3.json('/stats.json', function(data){
     let monGraph = data[2];
     console.log(monGraph);
-    let line1 = "M0," + monGraph.donnees1[0] * facteur1;
-    let line2 = "M0," + monGraph.donnees2[0] * facteur2;
-    monGraph.donnees1.forEach(function (d, i) {
+    let line1 = "M0," + monGraph.donnees1[0]*facteur1;
+    let line2 = "M0," + monGraph.donnees2[0]*facteur2;
+    monGraph.donnees1.forEach(function(d,i){
         svg.append("circle")
-            .attr('cx', i * 100)
-            .attr('cy', d * facteur1)
+            .attr('cx', i*100)
+            .attr('cy', d*facteur1)
             .attr('r', '3')
-            .attr('stroke', 'white');
-        line1 = line1 + "L" + i * 100 + "," + d * facteur1;
+            .attr('stroke', '#193549');
+            line1 = line1 + "L"+i*100+","+d*facteur1;
     });
     svg.append('path')
-        .attr('d', line1)
-        .style('stroke-width', '3')
-        .style('fill', 'none')
-        .attr('stroke', 'black');
+    .attr('d', line1)
+    .style('stroke-width', '3')
+    .style('fill', 'none')
+    .attr('stroke', 'black');
 
-    monGraph.donnees2.forEach(function (d, i) {
+    monGraph.donnees2.forEach(function(d,i){
         svg.append("circle")
-            .attr('cx', i * 100)
-            .attr('cy', d * facteur2)
+            .attr('cx', i*100)
+            .attr('cy', d*facteur2)
             .attr('r', '3')
-            .attr('stroke', 'white')
-            .attr('fill', 'red');
-        line2 = line2 + "L" + i * 100 + "," + d * facteur2;
+            .attr('stroke', '#193549')
+            .attr('fill', 'red')
+            .style('z-index', '10');
+            line2 = line2 + "L"+i*100+","+d*facteur2;
     });
     svg.append('path')
-        .attr('d', line2)
-        .style('stroke-width', '3')
-        .style('fill', 'none')
-        .attr('stroke', 'red');
+    .attr('d', line2)
+    .style('stroke-width', '3')
+    .style('fill', 'none')
+    .attr('stroke', 'red');
 
 
     let x = d3.axisBottom(d3.scaleLinear()
-        .domain([2010, 2016]) // la plage de valeurs possibles 
-        .range([0, 600])) // les coordonnées du début et de fin de l'axe
-        .ticks(7)
-        .tickFormat(d3.format('d'));
+    .domain([2010, 2016]) // la plage de valeurs possibles 
+    .range([0,  600])) // les coordonnées du début et de fin de l'axe
+    .ticks(7)
+    .tickFormat(d3.format('d'));
     svg.append("g")
-        .attr("transform", "translate(0,500)")
-        .call(x);
+    .attr("transform", "translate(0,500)")
+    .call(x);
 
+    let y1 = d3.axisLeft(d3.scaleLinear()
+    .domain([1.0, 3.0]) // la plage de valeurs possibles 
+    .range([500, 0])) // les coordonnées du début et de fin de l'axe
+    .ticks(10)
+    svg.append("g")
+    // .attr("transform", "translate(0,500)")
+    .call(y1);
 
+    let y2 = d3.axisRight(d3.scaleLinear()
+    .domain([7.0, 9.0]) // la plage de valeurs possibles 
+    .range([500, 0])) // les coordonnées du début et de fin de l'axe
+    .ticks(10)
+    svg.append("g")
+    .attr("transform", "translate(600,0)")
+    .call(y2);
 
     // let y1 = d3.scaleLinear()
     //   .domain( [1.0,3.0])
