019791ef598ce601e04105f48163e516103d6672
Valentin-Bleuse <98829094+Valentin-Bleuse@users.noreply.github.com>
2022-11-07 20:09:05 +0100

    mentions légales

---
+++ scripts/script-mentions-legales.js
@@ -0,0 +1,23 @@
+
+let etat = 0
+let slider = document.querySelector('.js-slider');
+let mentions = document.querySelector('.legal-container')
+function open_mentions() {
+    if (etat == 0) {
+        slider.style.display = 'none'
+        mentions.style.display = 'flex'
+        etat = 1
+    }
+    else {
+        slider.style.display = 'block'
+        mentions.style.display = 'none'
+        etat = 0
+
+    }
+}
+
+function close_mentions() {
+    slider.style.display = 'block'
+    mentions.style.display = 'none'
+    etat = 0
+}
